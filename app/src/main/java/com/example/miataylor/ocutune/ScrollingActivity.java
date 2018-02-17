3ec6c2479bb53cfcdc8c7cbb172f9aac2c98cb44
Mia R. Taylor <taylormr@vt.edu>
2018-02-17 12:17:59 -0500

    Changed lens to Morgan's version.

--- app/src/main/java/com/example/miataylor/ocutune/ScrollingActivity.java
+++ app/src/main/java/com/example/miataylor/ocutune/ScrollingActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -74,10 +78,22 @@ public class ScrollingActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(this.requestCode == requestCode && resultCode == RESULT_OK){
             Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            bitmap = getRoundedBitmap(bitmap);
             imageHolder.setImageBitmap(bitmap);
         }
 
 
+    }
+
+    public Bitmap getRoundedBitmap(Bitmap bitmap){
+        Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Paint paint = new Paint();
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+        Canvas c = new Canvas(circleBitmap);
+        c.drawCircle(bitmap.getWidth()/2, bitmap.getHeight()/2, bitmap.getWidth()/2, paint);
+        return circleBitmap;
     }
 
 
